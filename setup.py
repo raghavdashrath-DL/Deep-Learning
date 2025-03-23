9b636c9ad1f1c6657d3c33b0232babfb538590cd
raghavdashrath <raghavdashrath10@gmail.com>
2025-03-23 14:50:34 +0530

    Day 1 DL

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deep Learning with MLOps',
+    author='Raghav',
+    license='MIT',
+)
